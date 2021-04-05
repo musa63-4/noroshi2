32dea1ab35d194e72f42e5cbc376747e3505e08d
Musashi Akiyama <MUSAMUSA121863@gmail.com>
2021-04-05 20:49:41 +0900

    4/5_21

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 
 @app.route("/")
 def top():
     return render_template("no.html")
 
-@app.route("/")
+@app.route("/about.html")
 def about():
     return render_template("about.html")
 
-@app.route("/")
+@app.route("/members.html")
 def members():
     return render_template("members.html")
 
-@app.route("/")
+@app.route("/contact.html")
 def contact():
     return render_template("contact.html")
 
